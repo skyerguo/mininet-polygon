57f825ac7378c2e4eed4ff7af912698efb311b38
skyerguo97 <skyerguo97@gmail.com>
2021-08-18 21:15:33 +0800

    将代码改为循环

--- py-scripts/test_mininet.py
+++ py-scripts/test_mininet.py
@@ -60,8 +60,6 @@ def myNetwork():
     info( '*** Starting switches\n')
     ## 定义网卡
     for client_id in range(CLIENT_NUMBER):
-        # client[0].cmd('ifconfig client0-eth0 0')
-        # client[1].cmd('ifconfig client1-eth0 0')
         client[client_id].cmd('ifconfig client%s-eth0 0'%str(client_id))
 
     for switch_id in range(SWITCH_NUMBER):
@@ -72,44 +70,31 @@ def myNetwork():
     info( '*** Post configure switches and hosts\n')
     ## 对具体的网卡指定对应的ip
     for client_id in range(CLIENT_NUMBER):
-        # client[0].cmd('ifconfig client0-eth0 10.0.0.1/24')
-        # client[1].cmd('ifconfig client1-eth0 10.0.1.1/24')
         client[client_id].cmd('ifconfig client%s-eth0 10.0.%s.1/24'%(str(client_id), str(client_id)))
     
     for switch_id in range(SWITCH_NUMBER):
         for client_id in range(CLIENT_NUMBER):
-            # switch[switch_id].cmd('ifconfig switch%s-eth%s 0'%(str(switch_id), str(port_id)))
             switch[switch_id].cmd('ifconfig switch%s-eth%s 10.0.%s.2/24'%(str(switch_id), str(client_id), str(client_id)))
-    # switch[0].cmd('ifconfig switch0-eth0 10.0.0.2/24')
-    # switch[0].cmd('ifconfig switch0-eth1 10.0.1.2/24')
 
 
     ## 对具体的单节点网卡之间通信的测试 
     switch[0].cmd('route add -net 10.0.0.2 gw 10.0.1.2')
 
     ## 添加规则，从哪个ip来的，使用哪个路由表
-    client[0].cmd("ip rule add from 10.0.0.1 table 1")
-    client[1].cmd("ip rule add from 10.0.1.1 table 2")
-    switch[0].cmd("ip rule add from 10.0.0.2 table 11")
-    switch[0].cmd("ip rule add from 10.0.1.2 table 11")
+    for client_id in range(CLIENT_NUMBER):
+        client[client_id].cmd("ip rule add from 10.0.%s.1 table %s"%(str(client_id), str(client_id+1)))
+    
+    for switch_id in range(SWITCH_NUMBER):
+        for client_id in range(CLIENT_NUMBER):
+            switch[switch_id].cmd("ip rule add from 10.0.%s.2 table %s"%(str(client_id), str(switch_id+100)))
 
-    ## 添加规则，目的地址为xxx, dev表示用哪个网口，via表示下一跳的地址，scope表示适用范围
-    client[0].cmd("ip route add 10.0.0.0/24 dev client0-eth0 scope link table 1")
-    ## 并填入这个路由表的默认路由（其他情况的发包逻辑）
-    client[0].cmd("ip route add default via 10.0.0.2 dev client0-eth0 table 1") ## 可以去掉via？
-    client[0].cmd("ip route add default scope global nexthop via 10.0.0.2 dev client0-eth0") ## 可以把scope改为link？
-    ## 可以一个网关连入+连出吗？==> 得换ip网段，学着用
-    # switch0.cmd("ip route add 10.0.1.0/24 dev switch0-eth0 scope link table 11")
-    # switch0.cmd("ip route add default via 10.0.1.1 dev switch-eth0 table 11")
-    # switch0.cmd("ip route add default scope global nexthop via 10.0.1.1 dev switch-eth0")
-
-    client[1].cmd("ip route add 10.0.1.0/24 dev client1-eth0 scope link table 2")
-    client[1].cmd("ip route add default via 10.0.1.2 dev client1-eth0 table 2")
-    client[1].cmd("ip route add default scope global nexthop via 10.0.1.2 dev client1-eth0") 
-    # switch0.cmd("ip route add 10.0.0.0/24 dev switch0-eth1 scope link table 11")
-    # switch0.cmd("ip route add default via 10.0.0.1 dev switch-eth1 table 11")
-    # switch0.cmd("ip route add default scope global nexthop via 10.0.0.1 dev switch-eth1")
-
+    ## 添加节点上的路由下一跳规则，以及具体的每个路由表的操作
+    for client_id in range(CLIENT_NUMBER):
+        ## 添加规则，目的地址为xxx, dev表示用哪个网口，via表示下一跳的地址，scope表示适用范围
+        client[client_id].cmd("ip route add 10.0.%s.0/24 dev client%s-eth0 scope link table %s"%(str(client_id), str(client_id), str(client_id+1)))
+        ## 并填入这个路由表的默认路由（其他情况的发包逻辑）
+        client[client_id].cmd("ip route add default via 10.0.%s.2 dev client%s-eth0 table %s"%(str(client_id), str(client_id), str(client_id+1))) ## 可以去掉via？
+        client[client_id].cmd("ip route add default scope global nexthop via 10.0.%s.2 dev client%s-eth0"%(str(client_id), str(client_id))) ## 可以把scope改为link？
  
     CLI(net)
     net.stop()
