9e2d2717e581277a280f764ab0f060d67ab6b048
skyerguo97 <skyerguo97@gmail.com>
2022-07-27 22:44:10 +0800

    experiments done

--- bash-scripts/main_imbalanced.sh
+++ bash-scripts/main_imbalanced.sh
@@ -1,13 +1,22 @@
-cp ~/mininet-polygon/json-files/topo_imbalanced_case1.json ~/mininet-polygon/json-files/topo.json
-sleep 10
-sudo mn -c && cd ~/mininet-polygon/py-scripts && sudo timeout 5000 python3 main.py Polygon
-sleep 60
-sudo mn -c && cd ~/mininet-polygon/py-scripts && sudo timeout 5000 python3 main.py Anycast
-sleep 60
-sudo mn -c && cd ~/mininet-polygon/py-scripts && sudo timeout 5000 python3 main.py FastRoute
-sleep 60
+# cp ~/mininet-polygon/json-files/topo_imbalanced_case1.json ~/mininet-polygon/json-files/topo.json
+# sleep 10
+# sudo mn -c && cd ~/mininet-polygon/py-scripts && sudo timeout 5000 python3 main.py Polygon
+# sleep 60
+# sudo mn -c && cd ~/mininet-polygon/py-scripts && sudo timeout 5000 python3 main.py Anycast
+# sleep 60
+# sudo mn -c && cd ~/mininet-polygon/py-scripts && sudo timeout 5000 python3 main.py FastRoute
+# sleep 60
 
-cp ~/mininet-polygon/json-files/topo_imbalanced_case2.json ~/mininet-polygon/json-files/topo.json
+# cp ~/mininet-polygon/json-files/topo_imbalanced_case2.json ~/mininet-polygon/json-files/topo.json
+# sleep 10
+# sudo mn -c && cd ~/mininet-polygon/py-scripts && sudo timeout 5000 python3 main.py Polygon
+# sleep 60
+# sudo mn -c && cd ~/mininet-polygon/py-scripts && sudo timeout 5000 python3 main.py Anycast
+# sleep 60
+# sudo mn -c && cd ~/mininet-polygon/py-scripts && sudo timeout 5000 python3 main.py FastRoute
+# sleep 60
+
+cp ~/mininet-polygon/json-files/topo_imbalanced_case3.json ~/mininet-polygon/json-files/topo.json
 sleep 10
 sudo mn -c && cd ~/mininet-polygon/py-scripts && sudo timeout 5000 python3 main.py Polygon
 sleep 60
@@ -17,14 +26,3 @@ sudo mn -c && cd ~/mininet-polygon/py-scripts && sudo timeout 5000 python3 main.
 sleep 60
 
 sudo mn -c
-
-# cp ~/mininet-polygon/json-files/topo_imbalanced_case3.json ~/mininet-polygon/json-files/topo.json
-# sleep 10
-# sudo mn -c && cd ~/mininet-polygon/py-scripts && sudo timeout 5000 python3 main.py Polygon
-# sleep 60
-# sudo mn -c && cd ~/mininet-polygon/py-scripts && sudo timeout 5000 python3 main.py Anycast
-# sleep 60
-# sudo mn -c && cd ~/mininet-polygon/py-scripts && sudo timeout 5000 python3 main.py FastRoute
-# sleep 60
-
-# sudo mn -c
